4a5e94d81dfee478fa18574fd00b566357e2ce52
Mário Giusti <giustirmario@gmail.com>
2025-04-24 01:18:39 -0300

    Questoes adicionadas

---
+++ questao15.py
@@ -0,0 +1,4 @@
+m = float(input("Digite o valor do menor lado (em centimetros): "))
+M = float(input("Digite o valor do maior lado (em centimetros): "))
+perimetro = 2 * (m + M)
+print(f"O perímetro do retângulo/paralelograma é:", perimetro, "cm")
