6cf422bf09616718da7389cc69dec36836d7b5f9
Mário Giusti <giustirmario@gmail.com>
2025-04-24 01:40:16 -0300

    Nova questao

---
+++ questao17.py
@@ -0,0 +1,4 @@
+Q = float(input("Digite a quanidade de lados do poligono: "))
+L = float(input("Digite o valor do lado do poligono (em centimetros): "))
+perimetro = Q * L
+print(f"O perímetro do poligono(regular) é:", perimetro, "cm")
